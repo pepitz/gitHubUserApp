114feb19ecc9770f32757664b1b51a29f1beeb39
Petar Tsenev <petar.tzenev@gmail.com>
2019-01-14 02:00:02 +0100

    Initial structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import baseStyles from '../src/Assets/styles/base.module.css';
+import globalStyles from '../src/Assets/global-styles/bootstrap.min.module.css';
+import cx from 'classnames';
+
+import Header from './Header/header';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={cx(baseStyles.base, globalStyles['container-fluid'])}>
+        <Header />
+        <div className="search row">
+          <div className="col-lg-6">Users</div>
+          <div className="col-lg-6">Organiszations</div>
+        </div>
+      
       </div>
     );
   }
---
+++ src/Header/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import classes from './header.module.css';
+import globalStyles from '../Assets/global-styles/bootstrap.min.module.css';
+import cx from 'classnames';
+
+const header = () => {
+  return (
+    <header className={cx(classes.header, globalStyles.row)}>
+      <span className={cx(classes.header__logo, globalStyles['col-md-2'])}>Logo
+      </span>
+      <div className={cx(classes.header__text, globalStyles['col-md-9'])}>Github User App</div>
+    </header>
+  );
+};
+
+export default header;
